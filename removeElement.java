969a44e2b8b06a4f3e29fa0c970cf2afe5f92ca7
Patel Vaibhav <145320728+vaibhav-patel-1211@users.noreply.github.com>
2024-02-04 10:49:38 +0530

    Update removeElement.java

--- removeElement.java
+++ removeElement.java
@@ -42,6 +42,25 @@ class Solution {
         return j;
 
     }
+    // solution 2: 
+    int removeElement2(int arr[]) {
+        int n = arr.length;
+        int j = 1;
+
+        for (int i = 1; i < n - 1; i++) {
+            if (arr[i] != arr[i - 1]) {
+                arr[j] = arr[i];
+                j++;
+            }
+
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        return j;
+    }
 
     public static void main(String[] args) {
         Solution s = new Solution();
